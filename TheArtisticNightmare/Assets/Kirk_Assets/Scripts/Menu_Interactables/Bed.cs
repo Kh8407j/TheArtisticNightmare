3e3f85dc13a29c6f3367d465af82303dd69bd0f0
Kh8407j <kh8407j@gre.ac.uk>
2023-11-17 03:42:54 +0000

    Menu Progress

---
+++ TheArtisticNightmare/Assets/Kirk_Assets/Scripts/Menu_Interactables/Bed.cs
@@ -0,0 +1,27 @@
+// KHOGDEN 001115381
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace menuInteractable
+{
+    public class Bed : MenuInteractable
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public override void OnClick()
+        {
+            base.OnClick();
+        }
+    }
+}
---
+++ TheArtisticNightmare/Assets/Kirk_Assets/Scripts/Menu_Interactables/MenuInteractable.cs
@@ -0,0 +1,28 @@
+// KHOGDEN 001115381
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace menuInteractable
+{
+    public class MenuInteractable : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        // KH - Called when this interactable is clicked on.
+        public virtual void OnClick()
+        {
+
+        }
+    }
+}
