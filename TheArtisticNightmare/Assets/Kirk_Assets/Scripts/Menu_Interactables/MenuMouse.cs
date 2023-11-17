bc68772e84b9e3e611cd4a7e6e15ba0cae90bebb
Kh8407j <kh8407j@gre.ac.uk>
2023-11-17 10:42:27 +0000

    Coding progress

--- TheArtisticNightmare/Assets/Kirk_Assets/Scripts/Menu_Interactables/MenuMouse.cs
+++ TheArtisticNightmare/Assets/Kirk_Assets/Scripts/Menu_Interactables/MenuMouse.cs
@@ -3,12 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace menuInteractable
+namespace UI
 {
-    public class MenuInteractable : MonoBehaviour
+    public class MenuMouse : MonoBehaviour
     {
-        [SerializeField] string messageOnMouseHover;
-
         // Start is called before the first frame update
         void Start()
         {
@@ -17,12 +15,6 @@ namespace menuInteractable
 
         // Update is called once per frame
         void Update()
-        {
-
-        }
-
-        // KH - Called when this interactable is clicked on.
-        public virtual void OnClick()
         {
 
         }
